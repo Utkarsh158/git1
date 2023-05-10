5318cdd4ac63b44c89553a5609da1574c85526e8
Utkarsh <utkarshrajput1583@gmail.com>
2023-05-10 17:48:28 +0530

    br

--- src/components/comp1.js
+++ src/components/comp1.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import './comp1.css';
 function Comp1(props) {
-    const [title,setTitle] = useState(props.title);
+    const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
-        setTitle(props.title+' Updated!');
-        console.log(title);
+        setTitle(props.title + ' Added!');
+    };
+    const clickHandler2 = () => {
+        setTitle(props.title + ' Removed!');
     };
 
     return (<div className="product">
         <h2>{title}</h2>
         <p className="price">{props.price}</p>
         <p>{props.description}</p>
-        <button onClick={clickHandler}>Click Me!</button>
+        <button onClick={clickHandler} className='btn'>ADD..</button>
+        &nbsp;&nbsp;
+        <button onClick={clickHandler2} className='btn'>REMOVE..</button>
     </div>);
 }
 
