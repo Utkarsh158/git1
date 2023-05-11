0eb43c5fc0a152ee634dbea2387039f238e20230
Utkarsh <utkarshrajput1583@gmail.com>
2023-05-11 11:58:52 +0530

    he

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Product from './components/Product';
-
+import ShopInput from './components/ShopInput';
 
 function App() {
   const products = [
     { title: "Mouse", price: 600, description: "First product" },
-    { title: "Keyboard", price: 1200, description: "Second product" },
+    { title: "Key", price: 1200, description: "Second product" },
     { title: "Monitor", price: 1500, description: "Third product" },
     { title: "Printer", price: 2500, description: "Fourth product" },
     { title: "Scanner", price: 3500, description: "Fifth product" },
@@ -14,6 +14,8 @@ function App() {
 
   return (
     <div className='pro'>
+    <h1 align="center" style={{ fontSize: '2.5rem' }}>Enter Details</h1>
+        <ShopInput />
       <h1 align="center" style={{ fontSize: '2.5rem' }}>My Shop</h1>
       <Product items={products} />
     </div>
